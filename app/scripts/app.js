f8b7e45c02935e58ae01e6912c35cb1fd09ff153
umairkhan14 <umairkhan14@outlook.com>
2017-08-27 21:16:31 +0500

    content in individual pages updated & states added via ui-router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,52 @@
-var app = angular.module('quizApp', []);
+var app = angular.module('designApp', ['ui.router']).config(function($stateProvider, $urlRouterProvider) {
+
+
+	$stateProvider.state('app', {
+		url: '/',
+
+		views: {
+			'header': {
+				templateUrl: 'views/header.html'
+			},
+
+			'content': {
+				templateUrl: 'views/home.html',
+				controller: 'HomeController'
+			},
+
+			'footer': {
+				templateUrl: 'views/footer.html'
+			}
+		}
+	}).state('app.about', {
+		url: 'about',
+
+		views: {
+
+			'content@': {
+				templateUrl: 'views/about.html',
+				controller: 'AboutController'
+			}
+		}
+	}).state('app.services', {
+		url: 'services',
+
+		views: {
+			'content@': {
+				templateUrl: 'views/services.html',
+				controller: 'ServicesController'
+			}
+		}
+	}).state('app.contact', {
+		url: 'contact', 
+
+		views: {
+			'content@': {
+				templateUrl: 'views/contact.html',
+				controller: 'ContactController'
+			}
+		}
+	});
+
+	$urlRouterProvider.otherwise('/');
+});
